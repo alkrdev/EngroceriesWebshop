7c2ff2f87433117cf7b2dff9e3122e1ee2dbcdfa
Kasper <kaha.skp@edu.mercantec.dk>
2021-04-13 09:58:36 +0200

    WIP

--- public/index.php
+++ public/index.php
@@ -77,9 +77,8 @@ $db->connect();
 </main>
 <footer></footer>
 
-<script src="front-end/js/vendor/modernizr-3.11.2.min.js"></script>
-<script src="front-end/js/plugins.js"></script>
-<script src="front-end/js/main.js"></script>
+<script src="js/vendor/modernizr-3.11.2.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 
 </html>
