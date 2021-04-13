c0926ac1f05d0b92aee0403704efa23c15e3a459
Kasper <kaha.skp@edu.mercantec.dk>
2021-04-13 09:52:19 +0200

    structure change for routing WIP

--- public/index.php
+++ public/index.php
@@ -77,9 +77,9 @@ $db->connect();
 </main>
 <footer></footer>
 
-<script src="js/vendor/modernizr-3.11.2.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+<script src="front-end/js/vendor/modernizr-3.11.2.min.js"></script>
+<script src="front-end/js/plugins.js"></script>
+<script src="front-end/js/main.js"></script>
 </body>
 
 </html>
