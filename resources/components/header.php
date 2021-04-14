2410740af429812950ac8500825912e292de8510
Alexander <math828f@edu.mercantec.dk>
2021-04-14 10:13:18 +0200

    Remove Irrelevant navigation elements, and center Search

--- resources/components/header.php
+++ resources/components/header.php
@@ -1,8 +1,7 @@
 <header>
     <h1>Engroceries</h1>
-    <nav>
-        <a href="#register.html">Registrer</a>
-        <a href="#login.html">Log Ind</a>
-    </nav>
     <input type="text" name="search" id="search">
+    <nav>
+        <!-- Nav Elements -->
+    </nav>
 </header>
