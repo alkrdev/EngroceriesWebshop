65035fb2ea6a3d9e8b50d4fefa7424dfbabc7049
Kasper <kaha.skp@edu.mercantec.dk>
2021-04-15 14:35:18 +0200

    Products are now shown on the shop page after you are logged in

--- resources/views/Shop.php
+++ resources/views/Shop.php
@@ -1,6 +1,6 @@
 <?php
-    /** @var array $params */
-    define('Products', $params);
+/** @var array $params */
+define('Products', $params);
 ?>
 <!doctype html>
 <html class="no-js" lang="da-DK">
@@ -24,147 +24,50 @@
 
     <link type="text/css" rel="stylesheet" href="<?= asset('css/normalize.css') ?>">
     <link type="text/css" rel="stylesheet" href="<?= asset('css/main.css') ?>">
-	    <link type="text/css" rel="stylesheet" href="<?= asset('css/shop.css') ?>">
+    <link type="text/css" rel="stylesheet" href="<?= asset('css/shop.css') ?>">
 
     <meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
-    <header>
-        <h1>Engroceries</h1>
-        <nav>
-         
-        </nav>
-        <input type="text" name="search" id="search">
-    </header>
-    <main>
-        <section>    
-            <div class="filter-container">
-                <h1>Filter <h1>
-            
+<header>
+    <h1>Engroceries</h1>
+    <nav>
 
-            </div>
+    </nav>
+    <input type="text" name="search" id="search">
+</header>
+<main>
+    <section>
+        <div class="filter-container">
+            <h1>Filter <h1>
 
-            <div class="productcontainer" > 
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>  
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>  
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>  
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>  
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>  
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>  
-                <div class="product">
-                    <img src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&amp;ssl=1" style="width: 140px;">
-                    <h2> Rødt æble </h2>
-                    <p>1 stk. ca. 3 kg / Spanien / Klasse 1</p>
-                    <p class="price"> Pris 20.00 </p>
-                    <p>6,6 per kg  </p>
-                    <p> Varenr: 1231231 </p>
-                </div>      
-            </div>
-        </section>
-    </main>
-    <footer></footer>
 
-    <script src="<?= asset('js/vendor/modernizr-3.11.2.min.js') ?>"></script>
-    <script src="<?= asset('js/main.js') ?>"></script>
+        </div>
+
+        <div class="productcontainer">
+            <?php
+            foreach (Products as $product) {
+                ?>
+                <div class="product">
+                    <img src="<?= '/images/' . $product['image'] ?>"
+                         style="width: 140px;">
+                    <h2> <?= $product['name'] ?> </h2>
+                    <p><?= $product['description'] ?></p>
+                    <p class="price">Pris kr. <?= $product['price_per_unit'] ?>,- </p>
+                    <p> Varenr: <?= $product['product_number'] ?> </p>
+                </div>
+                <?php
+            }
+            ?>
+          
+        </div>
+    </section>
+</main>
+<footer></footer>
+
+<script src="<?= asset('js/vendor/modernizr-3.11.2.min.js') ?>"></script>
+<script src="<?= asset('js/main.js') ?>"></script>
 </body>
 
 </html>
