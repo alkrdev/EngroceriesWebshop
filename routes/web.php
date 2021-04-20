d815fcb3b4dc9be2d6982b97c44aff734503d504
Kasper <kaha.skp@edu.mercantec.dk>
2021-04-20 13:31:57 +0200

    Added user controller + register function
    Added and moved relevant functions to UserController + edited routes to match
    created send registration function

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,13 @@ use Pecee\SimpleRouter\SimpleRouter as Router;
 Router::get('/', 'Homecontroller@index');
 
 //Login
-Router::get('/login','HomeController@login');
-Router::post('/login', 'HomeController@login')->name('login');
-
+Router::get('/login','UserController@login');
+Router::post('/login', 'UserController@login')->name('login');
 //register
-Router::get('/register', 'HomeController@register')->name('register');
-Router::post('/register', 'HomeController@register')->name('register');
+Router::get('/register', 'UserController@register');
+Router::post('/register', 'UserController@SendApplication')->name('register');
 
-Router::post('/', 'HomeController@logout')->name('logout');
+Router::post('/', 'UserController@logout')->name('logout');
 
 Router::get('/orders', 'HomeController@orders')->name('orders');
 Router::get('/storage', 'HomeController@storage')->name('storage');
@@ -19,6 +18,7 @@ Router::get('/adminpanel', 'HomeController@adminpanel')->name('adminpanel');
 Router::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
 
 
+//Shop
 Router::group(['prefix' => '/shop'], function () {
     Router::get('/', 'HomeController@Shop')->name('Shop');
     Router::get('/product/{id?}', 'HomeController@getProduct');
