d4a28459157971f5d01c7042c46016cf2e198dcf
Kasper <kaha.skp@edu.mercantec.dk>
2021-04-20 15:31:02 +0200

    formatting changes Register.php

--- resources/views/Register.php
+++ resources/views/Register.php
@@ -22,33 +22,30 @@ include_once(component("header.php"));
                 <input type="text" name="name" id="name" placeholder="Navn" required>
 
                 <input type="password" name="psw" id="pswregister" placeholder="Kode" required>
-                             <span><?php if (isset($error_message)) echo $error_message?></span>
-                        <span class="validate">Koden skal indeholde følgende:
+                <span><?php if (isset($error_message)) echo $error_message ?></span>
+                <span class="validate">Koden skal indeholde følgende:
                         <br>- 8 tegn
                         <br>- mindst ét stort bogstav
                         <br>- mindst ét lille bogstav
                         <br>- mindst ét tal
                         <br>- mindst ét symbol</span>
 
-                        <input type="password" name="psw-repeat" id="pswregisterrepeat" placeholder="Kode igen"
-                               required>
-                        <span class="validate">Gentag koden præcist</span>
+                <input type="password" name="psw-repeat" id="pswregisterrepeat" placeholder="Kode igen" required>
+                <span class="validate">Gentag koden præcist</span>
 
-                        <input type="number" maxlength="8" name="phone-number" id="phone-number" placeholder="Telefon"
-                               required>
+                <input type="number" maxlength="8" name="phone-number" id="phone-number" placeholder="Telefon" required>
 
-                        <input type="email" name="email" id="emailregister" placeholder="Email" required>
-                        <span class="validate">Venligst indtast en korrekt email</span>
+                <input type="email" name="email" id="emailregister" placeholder="Email" required>
+                <span class="validate">Venligst indtast en korrekt email</span>
 
-                        <input type="number" maxlength="4" name="zip-code" id="zip-code" placeholder="Post Nummer"
-                               required>
+                <input type="number" maxlength="4" name="zip-code" id="zip-code" placeholder="Post Nummer" required>
 
-                        <input type="text" name="address" id="address" placeholder="Addresse" required>
+                <input type="text" name="address" id="address" placeholder="Addresse" required>
 
-                        <input type="text" name="cvr" id="cvrregister" placeholder="CVR Nummer" required>
-                        <span class="validate">CVR Nummeret skal være 8 cifre</span>
+                <input type="text" name="cvr" id="cvrregister" placeholder="CVR Nummer" required>
+                <span class="validate">CVR Nummeret skal være 8 cifre</span>
 
-                        <button type="submit" name="registration-submit" id="registration-submit" value="Register">Indsend Ansøgning</button>
+                <button type="submit" name="registration-submit" id="registration-submit" value="Register">Indsend Ansøgning</button>
             </form>
         </article>
 
