38912e1faab03bb4bc2ce2732a4bdd21be77adbc
Alexander <math828f@edu.mercantec.dk>
2021-04-26 02:13:17 +0200

    Additional Refactoring

--- engrocerieswebshopreact/src/components/Register.jsx
+++ engrocerieswebshopreact/src/components/Register.jsx
@@ -1,5 +1,44 @@
 import React from "react";
 
+// Source: https://stackoverflow.com/a/46181
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+$("#pswregister").on("change", function() {
+    var regex = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])[a-zA-Z0-9!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]{8,}$/
+    var span = $(this).next();
+
+    if (this.value.match(regex)) {
+        span.hide();
+    } else {
+        span.show();
+    }
+})
+
+$("#pswregisterrepeat").on("change", function() {
+    var span = $(this).next();
+    var value = $("#pswregister").val();
+
+    if (this.value === value) {
+        span.hide();
+    } else {
+        span.show();
+    }
+})
+
+$("#cvrregister").on("change", function() {
+    var span = $(this).next();
+    var regex = /^\d{8}$/
+
+    if (this.value.match(regex)) {
+        span.hide();
+    } else {
+        span.show();
+    }
+})
+
 const Register = () => {
     return (
         <section id="register">
