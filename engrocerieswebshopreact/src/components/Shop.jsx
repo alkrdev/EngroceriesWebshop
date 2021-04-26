d2205c8b78cf46af840bc08f9d15e5ee4995710b
Alexander <math828f@edu.mercantec.dk>
2021-04-26 08:49:15 +0200

    Handle Product showcase

--- engrocerieswebshopreact/src/components/Shop.jsx
+++ engrocerieswebshopreact/src/components/Shop.jsx
@@ -1,63 +1,46 @@
 import React, { useEffect, useState } from "react";
 
-// $(".product").click(function ()
-// {
-//     $(location).attr("href", "/shop/product/" + this.dataset.id);
-// })
-
 const Shop = () => {
     const storedJwt = localStorage.getItem('token'); 
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {        
-        if (storedJwt == null || storedJwt == "") {
-            //console.log("NOT LOGGED IN, PUSHED TO LOG IN")
-            
-        } else {
-            fetch("http://localhost:5000/products", {                
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + storedJwt
-                }
-            }).then(res => res.json())
-            .then(json => {
-                setProducts(json)
-                setLoading(false)
-            })
-        }
+    useEffect(() => {      
+        fetch("http://localhost:5000/products", {                
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + storedJwt
+            }
+        }).then(res => res.json())
+        .then(json => {
+            setProducts(json)
+            setLoading(false)
+        })
     }, [])
 
     return (
         <section id="shop">
             <div id="toolbar">
                 <h2>Sorter efter:</h2>
-                <form id="order-by" action="<?= url('highPrice')?>" method="post">
-                    <button className="price-sort"  type="submit" name="shophigh" id="shophigh" value="shophigh">Pris: Høj til lav</button>
-                </form>
-                <form id="order-by" action="<?= url('lowPrice')?>" method="post">
-                    <button className="price-sort"  type="submit" name="lowPrice" id="lowPrice" value="lowPrice" >Pris: Lav til Høj </button>
-                </form>
-                <form id="order-by" action="<?= url('az')?>" method="post">
-                    <button className="price-sort"  type="submit" name="az" id="az" value="az">A-Z</button>
-                </form>
-                <form id="order-by" action="<?= url('za')?>" method="post">
-                    <button className="price-sort"  type="submit" name="az" id="az" value="az">Z-A</button>
-                </form>
+                <button className="price-sort"  type="submit" name="shophigh" id="shophigh" value="shophigh">Pris: Høj til lav</button>
+                <button className="price-sort"  type="submit" name="lowPrice" id="lowPrice" value="lowPrice" >Pris: Lav til Høj </button>
+                <button className="price-sort"  type="submit" name="az" id="az" value="az">A-Z</button>
+                <button className="price-sort"  type="submit" name="az" id="az" value="az">Z-A</button>
             </div>
 
             <div className="productcontainer">
-
-                <div className="product" data-id="<?=$product['product_number']?>">
-                    <img src="/images/2eb.jpg" alt="productimage"></img>
-                    <div id="productdescription">
-                        <h2> name </h2>
-                        <p>description...</p>
-                        <p className="price">Pris kr. price_per_unit,- </p>
-                        <p> Varenr: product_number </p>
-                    </div>
-                </div>                
+                {!loading ? products.map((product) => {
+                    return <div key={product.id} className="product" onClick={() => window.location.href = '/shop/product/' + product.id}>
+                        <img src={"/images/" + product.image} alt="productimage"></img>
+                        <div>
+                            <h2>{product.name}</h2>
+                            <p>{product.description.length > 25 ? product.description.substring(0, 45) + "..." : product.description}</p>
+                            <p>{"Pris kr. " + product.price_per_unit + ",-"}</p>
+                            <p>{"Varenr: " + product.product_number}</p>
+                        </div>
+                    </div>  
+                }) : <React.Fragment></React.Fragment>}              
             </div>
         </section>
     )
--- engrocerieswebshopreact/src/components/shop/Product.jsx
+++ engrocerieswebshopreact/src/components/shop/Product.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 
 const Product = () => {
     let { id } = useParams();
+    const storedJwt = localStorage.getItem('token'); 
+    const [product, setProduct] = useState({});
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {          
+        fetch("http://localhost:5000/getProductById", {                
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + storedJwt
+            },
+            body: JSON.stringify({ id })
+        }).then(res => res.json())
+        .then(json => {
+            setProduct(json[0])
+            setLoading(false)
+        })
+    }, [])
     
     return (
         <section id="product">
-            <img src="/images/2eb.jpg"></img>
+            <img src={"/images/" + product.image}></img>
 
             <aside>
-                <h1>name</h1>
-                <p>{"item: " + id}</p>
-                <h4>Pris kr. price_per_unit,-</h4>
-                <p>Beskrivelse: description</p>
-                <input type="number" id="quantity" name="quantity" min="1" max="5"></input>
-                
-                <button className="bestil" type="submit">Bestil</button>
+                <React.Fragment>
+                    <h1>{product.name}</h1>
+                    <p>{"item: " + product.id}</p>
+                    <h4>{"Pris kr. " + product.price_per_unit + ",-"}</h4>
+                    <p>{"Beskrivelse: " + product.description}</p>
+                    <input type="number" id="quantity" name="quantity" min="1" max="5"></input>
+                    
+                    <button className="bestil" type="submit">Bestil</button>
+                </React.Fragment>
             </aside>
         </section>
     )
